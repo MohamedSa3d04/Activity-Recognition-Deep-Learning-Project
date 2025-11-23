a0f602bd5dd06b2bb0cc48b103eb5bfa1f7db126
Mohamed Saad <bnsa3d2004@gmail.com>
2025-11-23 20:20:30 +0200

    V1 of BL1

--- base_line1.py
+++ base_line1.py
@@ -4,7 +4,9 @@ from torchvision.transforms import transforms
 from torch.utils.data import Dataset, DataLoader
 from data_utilites import get_frame_paths
 from PIL import Image
+import os
 
+from torchvision.models import resnet50
 
 class Data_Loader_BL1(Dataset):
     def __init__(self, main_path):
@@ -42,7 +44,7 @@ class Data_Loader_BL1(Dataset):
 
         # Read The Target
         class_ = self.categories_dct[target]
-        target = ([0] * 8)
+        target = [0] * 8
         target[class_] = 1
 
 
@@ -50,12 +52,81 @@ class Data_Loader_BL1(Dataset):
     
     
 def run(main_path):
+
+    # Let's dive in the training: 
+    n_epochs = 25
+
+    # - Loading the data
     dataset = Data_Loader_BL1(main_path)
-    data_loader = DataLoader(dataset, 30, shuffle=True) 
-    for frame_tensor, target in data_loader:
-        print(frame_tensor, target)
-        break
+    data_loader = DataLoader(dataset, 50, shuffle=True) 
 
-    print(frame_tensor.shape, target.shape)
+    # Folder to save Model Versions
+    folder_path = os.path.join(main_path, 'Models', 'BaseLine1')
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
 
+    # Set-Up The Training
+    # device:
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print("Using device:", device)
+
+    
+    # Model Fine-Tuined Resinet:
+
+    # Check If there is a checkpoint or not
+    model = resnet50(pretrained=True)
+    last_version_path = os.path.join(folder_path, f'LastVersion.pth')
+    if os.path.exists(last_version_path):
+        print("CheckPoint Existed!")
+        state_dict = torch.load(last_version_path, map_location=device)
+        model.load_state_dict(state_dict)
+
+    model.to(device)
+    model.fc = nn.Sequential(
+        nn.Linear(model.fc.in_features, 512),
+        nn.ReLU(),
+        nn.Dropout(0.3),
+        nn.Linear(512, 8)
+        ) # Replace last layer with Specific New 8 output classes
+
+    # Now let's freeze resinet layers except our new FC layers
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+
+    # unfreeze classifier
+    for parameter in model.fc.parameters():
+        parameter.requires_grad = True
+
+    # Optimizer + Crietrion
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+
+
+    # Training Loop
+    model.train()
+    for i in range(n_epochs):
+        for index, (frame_tensor, target) in enumerate(data_loader):
+            # get the output and calc the loss
+            output = model(frame_tensor.to(device)) 
+            loss = criterion(output, target.to(device))
+
+            # zero the optimizer (For not aggeregration)
+            optimizer.zero_grad()
+            loss.backward() # Compute gradients
+            optimizer.step() # Adjust learning weights
+
+            # report loss
+            running_loss += loss.item()
+            if index % 20 == 0: # For each 25 batches Report loss
+                last_loss = running_loss / 25 # loss per batch
+                print(f'Epoch {i}, Batch {index} -> {last_loss}')
+                running_loss = 0
+
+
+         # For each 5 epchs save a copy version
+        if i % 5 == 0:
+            torch.save(model.state_dict(), f=os.path.join(folder_path, f'V{i}.pth'))
+            torch.save(model.state_dict(), f=last_version_path) #Override Last Version
+          
 
